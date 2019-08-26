12b416eed3972e99787c187f1a8558d7d1a1b2a6
Paul Browne <Paulmatthewbrowne@gmail.com>
2019-08-26 20:29:37 +0300

    Create node-html-includes.js

---
+++ node-html-includes.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+
+function builder(input, output) {
+    output = output ? output : "";
+    var matches = input.match(/\[\[.*?\]\]/g);
+    if (matches) {
+        var path = matches[0].replace(/\]|\[/g, "");
+        var componentHTML = fs.readFileSync(path, 'utf8');
+        var newHTML = input.replace(matches[0], componentHTML);
+        return builder(newHTML, newHTML);
+    } else {
+        return output;
+    }
+}
+
+module.exports = function(arrayOfFiles){
+	arrayOfFiles.forEach(function(file) {
+	    var contents = fs.readFileSync(file, 'utf8');
+	    fs.writeFileSync(file, builder(contents))
+	})
+}
+
+// use like...
+// const builder = require('./node-html-includes');
+// builder(["index.html", "contact.html", "about.html"])
